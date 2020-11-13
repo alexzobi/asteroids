53da690f23708d93fcdcf10803703dcfbdb211b8
Alex <asobiloff@gmail.com>
2020-11-13 12:34:24 -0500

    add rn gesture handler and make ship move

--- src/components/Ship.tsx
+++ src/components/Ship.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Dimensions } from 'react-native';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  useAnimatedGestureHandler,
+  withDecay,
+} from 'react-native-reanimated';
+import { PanGestureHandler } from 'react-native-gesture-handler';
 
 const styles = StyleSheet.create({
   Ship: {
@@ -23,10 +30,50 @@ type Props = {
 
 }
 
-const Ship = ({}: Props) => (
-  <View style={styles.Ship}>
-    <View style={styles.Ship__Nose} />
-  </View>
-);
+const Ship = ({}: Props) => {
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+  const { width, height } = Dimensions.get('screen');
+
+  const onGestureEvent = useAnimatedGestureHandler({
+    onStart: (evt, ctx) => {
+      ctx.offsetX = translateX.value;
+      ctx.offsetY = translateY.value;
+    },
+    onActive: (evt, ctx) => {
+      translateX.value = ctx.offsetX + evt.translationX;
+      translateY.value = ctx.offsetY + evt.translationY;
+    },
+    onEnd: evt => {
+      translateX.value = withDecay({
+        velocity: evt.velocityX,
+        clamp: [0, width - 75], // optionally define boundaries for the animation
+      });
+      translateY.value = withDecay({
+        velocity: evt.velocityY,
+        clamp: [0, height - 75], // optionally define boundaries for the animation
+      });
+    },
+  });
+
+  const animationStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: translateX.value },
+        { translateY: translateY.value },
+      ]
+    }
+  });
+
+  return (
+    <PanGestureHandler {...{onGestureEvent}}>
+      <Animated.View style={animationStyle}>
+        <View style={styles.Ship}>
+          <View style={styles.Ship__Nose} />
+        </View>
+      </Animated.View>
+    </PanGestureHandler>
+  )
+};
 
 export default Ship;
